97dcde893788e2c6c6b358afbb3e4682476601aa
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2024-05-17 18:02:25 +0900

    Nx monorepo

--- apps/react-monorepo/src/app/app.tsx
+++ apps/react-monorepo/src/app/app.tsx
@@ -1,10 +1,18 @@
-import NxWelcome from './nx-welcome';
+import { Route, Routes } from 'react-router-dom';
+
+// importing the component from the library
+import { ProductList } from '@react-monorepo/products';
+
+function Home() {
+  return <h1>Home</h1>;
+}
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="react-monorepo" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<ProductList test={'test'} />}></Route>
+    </Routes>
   );
 }
 
---
+++ libs/products/src/lib/product-list/product-list.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable-next-line */
+export interface ProductListProps {
+  test: string;
+}
+
+export function ProductList(props: ProductListProps) {
+  return (
+    <div>
+      <h1>Welcome to ProductList!!@</h1>
+    </div>
+  );
+}
+
+export default ProductList;
